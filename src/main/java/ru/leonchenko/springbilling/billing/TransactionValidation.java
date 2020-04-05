f88a5bce656ae1325d71021d1d7f05b6f50115f6
Leo Kooper <lverixas@gmail.com>
2020-04-05 03:13:22 +0300

    local branch commit

--- src/main/java/ru/leonchenko/springbilling/billing/TransactionValidation.java
+++ src/main/java/ru/leonchenko/springbilling/billing/TransactionValidation.java
@@ -2,6 +2,8 @@ package ru.leonchenko.springbilling.billing;
 
 import ru.leonchenko.springbilling.entity.FinancialTransaction;
 
+import java.util.logging.Logger;
+
 /**
  * @author Igor Leonchenko
  * @version 1.0
@@ -9,18 +11,20 @@ import ru.leonchenko.springbilling.entity.FinancialTransaction;
 
 public class TransactionValidation {
 
-    public boolean validation(FinancialTransaction financialTransaction) {
+    private static Logger log = Logger.getLogger(TransactionValidation.class.getName());
+
+    public static boolean validation(FinancialTransaction financialTransaction) {
 
         if (financialTransaction.getId() > 0)
-            System.out.println("Id is valid");
+            log.info("Id is valid");
         else throw new IllegalArgumentException("Id is not valid");
 
         if (!financialTransaction.getSrcId().equals(financialTransaction.getDstId()))
-            System.out.println("Source and destination are valid");
+            log.info("Source and destination are valid");
         else throw new IllegalArgumentException("Source and destination are not valid");
 
         if (financialTransaction.getAmount() > 0)
-            System.out.println("Amount is valid");
+            log.info("Amount is valid");
         else throw new IllegalArgumentException("Amount is not valid");
 
         return true;
